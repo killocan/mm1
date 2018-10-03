249af053a939c36f2f4c3e45f932d124eecd1a5b
f6808754@bb.com.br <f6808754@bb.com.br>
2018-10-03 17:56:59 -0300

    removendo codigo comentado

--- text_utils.cpp
+++ text_utils.cpp
@@ -10,14 +10,12 @@
     
 void draw_text(BITMAP * buffer, FONT * mm_font, int x, int y, int fg, int bg, const char * str, int lenghtToPrint)
 {
-  //static const int charLen = text_length(mm_font, "A");
   char format[20] = {};
   sprintf(format, "%%.%ds", lenghtToPrint);
 
   for (unsigned int i = 0; i < strlen(str); i++)
   {
     textprintf_ex(buffer, mm_font, x, y, fg, bg, format, str);
-    //x+=charLen;
   }
 }
 
@@ -31,21 +29,6 @@ void draw_text_shadow(BITMAP * buffer, FONT * mm_font, int x, int y, int fg, con
 
 void draw_number(BITMAP * buffer, FONT * mm_font, int x, int y, int fg, int bg, int number)
 {
-  //int rndNumber = (rand()%10);
- // #define oht 100*1000
-
-  //if (number >= oht)
-  //{
-  //  rndNumber = (rndNumber*oht) + (number%oht);
   textprintf_ex(buffer, mm_font, x + 2, y + 2, 0, bg, "%d  ", number);
   textprintf_ex(buffer, mm_font, x, y, fg, -1, "%d  ", number);
- // }
- // else
- // {
-  //  textprintf_ex(buffer, mm_font, x + 2, y + 2, 0, bg, "%d", number);
-  //  textprintf_ex(buffer, mm_font, x, y, fg, -1, "%d", number);
-
-    //textprintf_ex(buffer, mm_font, x + 2, y + 2, 0, bg, "%d", rndNumber);
-    //textprintf_ex(buffer, mm_font, x, y, fg, -1, "%d", rndNumber);
-  //}
 }
