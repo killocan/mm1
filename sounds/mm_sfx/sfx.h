8172e3b2ea2695e814a1fdebf90d3352a19367f8
killocan <killocan@gmail.com>
2018-10-06 21:18:44 -0300

    arquivo gerado pelo grabber

---
+++ sounds/mm_sfx/sfx.h
@@ -0,0 +1,42 @@
+/* Allegro datafile object indexes, produced by grabber v4.4.2, Unix */
+/* Datafile: /home/marcos/megaman/sounds/mm_sfx/sfx.dat */
+/* Date: Sat Oct  6 21:17:21 2018 */
+/* Do not hand edit! */
+
+#define BOSS_SELECTED                    0        /* SAMP */
+#define PAUSE                            1        /* SAMP */
+#define MENU_ARROW                       2        /* SAMP */
+#define TELE                             3        /* SAMP */
+#define SAMPLE_04                        4        /* SAMP */
+#define MEGA_BUSTER                      5        /* SAMP */
+#define MM_HIT                           6        /* SAMP */
+#define LIGHTNING                        7        /* SAMP */
+#define SAMPLE_08                        8        /* SAMP */
+#define ENERGY_FILL                      9        /* SAMP */
+#define BONUS_POINT                      10       /* SAMP */
+#define NEW_LIFE                         11       /* SAMP */
+#define ENEMY_MEGA_BUSTER                12       /* SAMP */
+#define HIT_SHIELD                       13       /* SAMP */
+#define SAMPLE_14                        14       /* SAMP */
+#define SAMPLE_15                        15       /* SAMP */
+#define BIGEYEJUMP                       16       /* SAMP */
+#define SAMPLE_17                        17       /* SAMP */
+#define PLATFORM_MOVE                    18       /* SAMP */
+#define SAMPLE_19                        19       /* SAMP */
+#define SAMPLE_20                        20       /* SAMP */
+#define SAMPLE_21                        21       /* SAMP */
+#define DOOR                             22       /* SAMP */
+#define ROLLING_CUTTER                   23       /* SAMP */
+#define SUPER_ARM                        24       /* SAMP */
+#define SAMPLE_25                        25       /* SAMP */
+#define ICE_SLASHER                      26       /* SAMP */
+#define BOMBOMB_HIT                      27       /* SAMP */
+#define SAMPLE_28                        28       /* SAMP */
+#define FIRE_STORM                       29       /* SAMP */
+#define THUNDER_BEAM                     30       /* SAMP */
+#define HIT_ENEMY                        31       /* SAMP */
+#define MM_DIE                           32       /* SAMP */
+#define SAMPLE_33                        33       /* SAMP */
+#define MM_JUMP                          34       /* SAMP */
+#define MENU_BG                          35       /* SAMP */
+
