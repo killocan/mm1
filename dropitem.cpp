67645a8f5fe003da79b414a4f0583c6a198bb719
f6808754@bb.com.br <f6808754@bb.com.br>
2019-04-02 19:45:17 -0300

    spawn life and weapon recharge, new life and bonus points based on random

--- dropitem.cpp
+++ dropitem.cpp
@@ -10,7 +10,27 @@
 
 void DropItem::dropItem(Stage * stage, int x, int y)
 {
-  bool mortal = true;
-  TemporaryCharacterList::mm_tempCharacterLst.push_back(stage->createCharacter(mm_tile_actions::BONUS_POINT_CHAR, 
-                                                                               x, y, 0,0, &mortal));
+  int type = mm_tile_actions::BONUS_POINT_CHAR;
+
+  int chance = rand()%1000;
+  if (chance > 600)
+  {
+    if (chance > 970) type = mm_tile_actions::TILE_NEW_LIFE;
+    else
+    {
+      const int possibilities[] = {mm_tile_actions::TILE_BIG_LIFE_RECHARGE,
+                                   mm_tile_actions::TILE_BIG_WEAPON_RECHARGE,
+                                   mm_tile_actions::TILE_LITTLE_WEAPON_RECHARGE,
+                                   mm_tile_actions::TILE_LITTLE_WEAPON_RECHARGE,
+                                   mm_tile_actions::TILE_LITTLE_WEAPON_RECHARGE,
+                                   mm_tile_actions::TILE_LITTLE_LIFE_RECHARGE,
+                                   mm_tile_actions::TILE_LITTLE_LIFE_RECHARGE,
+                                   mm_tile_actions::TILE_LITTLE_LIFE_RECHARGE};
+
+      type = possibilities[rand() % 8];
+    }
+
+    bool mortal = true;
+    TemporaryCharacterList::mm_tempCharacterLst.push_back(stage->createCharacter(type, x, y, 0,0, &mortal));
+  }
 }
